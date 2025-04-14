dbd91055e1cf6fc0a05bf1d84249d1d31381ed17
IQRA BATOOL <Iqra.chaudhry78714@gmail.com>
2025-04-14 13:10:21 +0500

    Initial commit

---
+++ environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    backend: {
+      // baseURL: "https://api.deabt.us/",
+      baseURL: "http://localhost:5000/api/",
+      api: "api/",
+    },
+  };
+
